727dce960bdc8fb0bcf4e5a12875053f1c86f9ab
Jack Campbell <Jack Campbell>
2015-07-26 15:24:39 +0000

    helo

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
    validates :u_id, presence: true
     default_scope -> { order(created_at: :desc) }
   validates :description, presence: true, length: { maximum: 10000 }
+  validates :name, presence: true, length: { maximum: 10000 }
   belongs_to :u
   scope :for_u, lambda { |u| joins(:us).where("u_id = ?", u.id)}
     has_attached_file :pic, :styles => 
