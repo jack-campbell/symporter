4fa7c0ff1d3a49ceee346809344ba5a3c6830d1c
Jack Campbell <Jack Campbell>
2015-08-29 17:49:54 +0000

    fixed comma and paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Paperclip::Attachment.default_options[:url] = "/system/:class/:attachment/:id/:style/:filename"
 # Initialize the Rails application.
 Rails.application.initialize!
